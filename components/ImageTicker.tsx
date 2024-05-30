ae04b234bacf8d7301115516d5312827e816767e
Stefan Schernich <stefan.schernich@gmail.com>
2024-05-30 22:32:08 +0200

    Make ticker stop when hovered or touched

--- components/ImageTicker.tsx
+++ components/ImageTicker.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { motion, useTransform, useScroll, useSpring } from "framer-motion";
 import Ticker from "framer-motion-ticker";
 
@@ -7,9 +7,19 @@ export default function ImageTicker({ children }: { children: JSX.Element[] }) {
   const { scrollYProgress } = useScroll();
   const xShift = useTransform(scrollYProgress, [0, 1], [250, -250]);
   const smoothedXShift = useSpring(xShift, { stiffness: 100, damping: 20 });
+
+  const [isPlaying, setIsPlaying] = useState<boolean>(true);
   return (
-    <motion.div style={{ x: smoothedXShift }} className="">
-      <Ticker duration={30}>{children}</Ticker>
+    <motion.div
+      style={{ x: smoothedXShift }}
+      onHoverStart={() => setIsPlaying(false)}
+      onHoverEnd={() => setIsPlaying(true)}
+      onTouchStart={() => setIsPlaying(false)}
+      onTouchEnd={() => setIsPlaying(true)}
+    >
+      <Ticker duration={30} isPlaying={isPlaying}>
+        {children}
+      </Ticker>
     </motion.div>
   );
 }
