04a75957674f88dd37db583bfca8787378a9d54d
Stefan Schernich <stefan.schernich@gmail.com>
2024-05-30 14:44:36 +0200

    Add ticker to all devices, add faq

---
+++ components/FAQ.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+const faqs = [
+  {
+    value: "item-1",
+    triggerText: "Wie heiratet ihr?",
+    content:
+      "Nur standesamtlich. Danach möchten wir ganz entspannt ein bisschen mit euch feiern.",
+  },
+  {
+    value: "item-2",
+    triggerText: "Wollt ihr einen JGA?",
+    content:
+      "Nein, danke! Ist nicht unser Ding. Wie freuen uns stattdessen auf den 04.10. mit euch!",
+  },
+  {
+    value: "item-3",
+    triggerText: "Wie ist der Dresscode?",
+    content:
+      "Wäre schön, wenn ihr euch etwas schick macht. Wie man eben zu einer Hochzeit geht. Ihr müsst es aber nicht übertreiben!",
+  },
+  {
+    value: "item-4",
+    triggerText: "Was wünscht ihr euch als Geschenk?",
+    content: "Geld ist immer gut. 😉",
+  },
+  {
+    value: "item-5",
+    triggerText: "Kann man auch übernachten?",
+    content:
+      "Ja, der Herzogspark bietet für Hochzeitsgäste Zimmer für 115€ (EZ) bzw. 125€ (DZ) an.",
+  },
+];
+
+export default function FAQ() {
+  return (
+    <Accordion
+      className="m-auto w-full max-w-[600px] items-center rounded-lg bg-white px-4"
+      type="single"
+      collapsible
+    >
+      {faqs.map(({ value, triggerText, content }) => (
+        <AccordionItem
+          key={value}
+          value={value}
+          className="py-2 last:border-b-0"
+        >
+          <AccordionTrigger className="bold md:text-xl">
+            {triggerText}
+          </AccordionTrigger>
+          <AccordionContent className="text-balance text-gray-600 md:text-lg">
+            {content}
+          </AccordionContent>
+        </AccordionItem>
+      ))}
+    </Accordion>
+  );
+}
--- components/ImageTicker.tsx
+++ components/ImageTicker.tsx
@@ -3,21 +3,13 @@ import React from "react";
 import { motion, useTransform, useScroll, useSpring } from "framer-motion";
 import Ticker from "framer-motion-ticker";
 
-// the window resize hook is required to properly hide/show the ticker on smaller/larger screens. If it is just "hidden" via CSS on smaller screens, it is still rendered due to its implementation by the creator and will cause an RangeError.
-// The hook moves the screen-size check to the client and thus ensures that it is only rendered on larger screens
-import useWindowSize from "@/customHooks/useWindowSize";
-
 export default function ImageTicker({ children }: { children: JSX.Element[] }) {
   const { scrollYProgress } = useScroll();
-  const xShift = useTransform(scrollYProgress, [0, 1], [-250, 250]);
+  const xShift = useTransform(scrollYProgress, [0, 1], [250, -250]);
   const smoothedXShift = useSpring(xShift, { stiffness: 100, damping: 20 });
-  const { width } = useWindowSize();
-  const isLargeScreen = width && width >= 1024;
   return (
-    isLargeScreen && (
-      <motion.div style={{ x: smoothedXShift }}>
-        <Ticker duration={30}>{children}</Ticker>
-      </motion.div>
-    )
+    <motion.div style={{ x: smoothedXShift }} className="">
+      <Ticker duration={30}>{children}</Ticker>
+    </motion.div>
   );
 }
